e172ad64a320320bbfa278c47391e1e71e82e28e
IrenaStonkiene <irena.stonkiene@hotmail.com>
2022-05-04 21:14:37 +0300

    Created Product class

---
+++ Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    public class Product
+    {
+     
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public double VitaminAQuanity { get; set; }
+
+        public double VitaminB1Quanity { get; set; }
+
+        public double VitaminB2Quanity { get; set; }
+        public double VitaminB3Quanity { get; set; }
+        public double VitaminB5Quanity { get; set; }
+        public double VitaminB6Quanity { get; set; }
+        public double VitaminB7Quanity { get; set; }
+        public double VitaminB12Quanity { get; set; }
+        public double VitaminCQuanity { get; set; }
+        public double VitaminDQuanity { get; set; }
+        public double VitaminEQuanity { get; set; }
+
+    }
+
+}
