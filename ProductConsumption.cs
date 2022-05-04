ebeb61d92c8cb3084803f8c9e813cea0ce0d2e25
IrenaStonkiene <irena.stonkiene@hotmail.com>
2022-05-04 22:55:19 +0300

    Added User Input and new Class "ProductConsumption"

---
+++ ProductConsumption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    public class ProductConsumption
+    {
+        public Product Product { get; set; }
+        public double ConsumedQuantity { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Welcome to Optimal Nutrition");
+Console.WriteLine("____________________________");
+Console.WriteLine("Please, select product from the list by product ID");
+
+bool runProgram = true;
+
+while (runProgram)
+{
+    Console.WriteLine("{Productlist}");//Product list will be printed
+    Console.WriteLine("Enter product ID or press enter");
+    var userInput = Console.ReadLine();
+    runProgram = int.TryParse(userInput, out var productID);
+    if (runProgram)
+    {
+        Console.WriteLine("Enter consumed quantity in grams");
+        var productQuantity = Console.ReadLine();
+        var isProductQuantityNumber = double.TryParse(productQuantity, out double productQuantity1);
+        Console.WriteLine();
+    }
+
+}
+   
